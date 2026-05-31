c6dbae8a20ba8ae0bcccd8b346c4d9f086becb84
NayanJyotiKalita <nayanjyotikalita170@gmail.com>
2026-05-31 17:10:38 +0000

    New Filesf for the latest module added

---
+++ 05-ec2-with-sg-vpc-eip-exec/i4-02-vpc-module.tf
@@ -0,0 +1,42 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.6.1"
+
+  name             = "${local.name}-${var.vpc_name}"
+  cidr             = var.vpc_cidr_block
+  azs              = var.vpc_azs
+  public_subnets   = var.vpc_public_subnets
+  private_subnets  = var.vpc_private_subnets
+  
+  # Database Subnets
+  database_subnets                    = var.vpc_database_subnets
+  create_database_subnet_group        = var.vpc_create_database_subnet_group
+  create_database_subnet_route_table  = var.vpc_create_database_subnet_route_table
+  # This usage of variables replaces the use of writing the hardcoded values for each identifier
+  # e.g. : create_database_internet_gateway_route = true
+         # create_database_nat_gateway_route = true
+
+  # NAT Gateways - Outbound Communication (Single NAT in the VPC)
+  enable_nat_gateway     = var.vpc_enable_nat_gateway
+  single_nat_gateway     = var.vpc_single_nat_gateway
+  one_nat_gateway_per_az = var.vpc_one_nat_gateway_per_az
+
+  # VPC DNS Parameters (we can variablise these too)
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = local.common_tags
+  vpc_tags = local.common_tags
+
+  public_subnet_tags = {
+    type = "public-subnets"
+  }
+  
+  private_subnet_tags = {
+    type = "private-subnets"
+  }
+
+  database_subnet_tags = {
+    type = "database-subnets"
+  }
+}
---
+++ 05-ec2-with-sg-vpc-eip-exec/i8-elasticip.tf
@@ -0,0 +1,20 @@
+resource "aws_eip" "public_elastic_ip" {
+  # The elastic ip needs to be attached to the ec2 instance so it needs to wait till the instance comes up
+  # The eip also needs to send traffic outside the vpc via the igw so it needs to wait till the vpc with the igw to come up
+  depends_on = [ module.ec2_bastion, module.vpc ]  
+
+  instance   = module.ec2_bastion.id
+  domain     = "vpc"
+  tags       = local.common_tags
+
+
+  /*
+  provisioner "local-exec" {
+    command     = "echo Destroy time provider `date` >> destroy-time-provider.txt"
+    working_dir = "local-exec-output-files/"
+    when        = destroy
+    #on_failure = continue
+}
+*/
+}
+
